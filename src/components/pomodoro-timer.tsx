939d540c21021abb64859052696204135d6c9aaf
vinnilmg <vini.delimagomes@gmail.com>
2023-05-18 21:46:51 -0300

    seconds to time

--- src/components/pomodoro-timer.tsx
+++ src/components/pomodoro-timer.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useInterval } from '../hooks/use-interval';
+import { secondsToTime } from '../utils/seconds-to-time';
 
 interface Props {
   defaultPomodoroTimer: number;
@@ -12,5 +13,5 @@ export function PomodoroTimer(props: Props): JSX.Element {
     setMainTime(mainTime - 1);
   }, 1000);
 
-  return <div>Olá mundo {mainTime}</div>;
+  return <div>Olá mundo {secondsToTime(mainTime)}</div>;
 }
