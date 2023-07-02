5b123c650df58641acfd04a9290afef66aa8ac52
vinnilmg <vini.delimagomes@gmail.com>
2023-07-02 17:05:55 -0300

    Correcao Timer

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,8 +1,18 @@
-export function secondsToTime(seconds: number): string {
-  const zeroLeft = (n: number) => Math.floor(n).toString().padStart(2, '0');
+export function zeroLeft(n: number) {
+  return Math.floor(n).toString().padStart(2, '0');
+}
+
+export function secondsToMinutes(seconds: number): string {
   const min = zeroLeft((seconds / 60) % 60);
   const sec = zeroLeft((seconds % 60) % 60);
   return `${min}:${sec}`;
+}
+
+export function secondsToTime(seconds: number) {
+  const hours = zeroLeft(seconds / 3600);
+  const min = zeroLeft((seconds / 60) % 60);
+  const sec = zeroLeft((seconds % 60) % 60);
+  return `${hours}:${min}:${sec}`;
 }
 
 export function preencheQtdCiclos(qtdCiclos: number) {
